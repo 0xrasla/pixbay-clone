9e542ee08f3717de3ca186dbcb9d27660aa5fc76
Rasla <programmer.rasla@gmail.com>
2023-03-24 00:04:21 +0530

    a small documentation

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.scss";
+
+//theme
+import "primereact/resources/themes/mdc-dark-deeppurple/theme.css";
+
+//core
+import "primereact/resources/primereact.min.css";
+
+//icons
+import "primeicons/primeicons.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
